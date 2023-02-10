670980fec727ec4d9b0694555fc10db4900614c0
Abdulrahim Muhamed <121846308+AbdulRahim2204@users.noreply.github.com>
2023-02-10 21:36:38 +0200

    Update basicJavaScript.js

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -16,3 +16,32 @@
 var myName;
 myName = 'Abdulrahim';
 
+// switch case start :
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val){
+    case 1:
+    case 2:
+    case 3:
+      answer = 'Low';
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = 'Mid';
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = 'High';
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+// switch case end
