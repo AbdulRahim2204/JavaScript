eae3c2ad08c8c693ead1ab8973da9daaa66a6a50
Abdulrahim Muhamed <121846308+AbdulRahim2204@users.noreply.github.com>
2023-02-15 09:38:04 +0200

    Update basicJavaScript.js

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -74,3 +74,51 @@ function chainToSwitch(val) {
 
 chainToSwitch(7);
 // switch case end
+// album recorde
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(prop !== 'tracks' && value && !(records[id].hasOwnProperty(prop))){
+    records[id][prop] = value;
+    return records;
+
+  } else if(prop === 'tracks' && !(records[id].hasOwnProperty('tracks')) && value){
+    records[id][prop] = [value];
+    return records;
+
+  } else if(prop === 'tracks' && value){
+    records[id][prop].push(value);
+    return records;
+
+
+  } else if(!value){
+    delete records[id][prop];
+    return records;
+  }
+  
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+// end album records
